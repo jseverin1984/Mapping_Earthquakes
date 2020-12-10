9f5004f7111a0d1abb0e988cee93ed87abfef28b
Joshua Severin <69886887+jseverin1984@users.noreply.github.com>
2020-12-10 10:32:34 -0600

    added circlemarker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,1 +1,31 @@
-first 
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [34.0522, -118.2437],
+    zoom: 14
+});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our tile layer to the map.
+streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]);
+
+// Add a circle to the map for Los Angeles
+let circle = L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "red",
+    fillColor: "#FEFFC7" 
+});
+
+circle.addTo(map);
