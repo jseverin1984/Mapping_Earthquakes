7dbab998d37a5095dabd2ba97a6263241cf6ce7a
Joshua Severin <69886887+jseverin1984@users.noreply.github.com>
2020-12-10 14:33:09 -0600

    mapped a line

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,35 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [37.6213, -122.3790],
+    zoom: 5
+});
+
+let line = [
+    [33.9416, -118.4085],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    dashArray: "10 5 5 10",
+    weight: 4
+  }).addTo(map);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our tile layer to the map.
+streets.addTo(map);
+
+
